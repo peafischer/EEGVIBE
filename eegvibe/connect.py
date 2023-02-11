e3c190f0a17abe5bc2bd08d1c61e110c6230addf
harisorgn <organtzh@gmail.com>
2023-02-11 12:29:47 +0000

    add connect functions for `zmq.PUB` and `zmq.SUB`

---
+++ eegvibe/connect.py
@@ -0,0 +1,16 @@
+import zmq
+
+def generate_publisher(port, context):
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://*:%s" % port)
+
+    return socket
+
+def generate_subscriber(port, topic, context):
+    socket = context.socket(zmq.SUB)
+    socket.connect(f"tcp://localhost:{port}")
+    topic_filter = topic.encode('utf-8')
+    socket.setsockopt(zmq.SUBSCRIBE, topic_filter)
+    socket.setsockopt(zmq.SUBSCRIBE, ''.encode('utf-8'))
+
+    return socket
