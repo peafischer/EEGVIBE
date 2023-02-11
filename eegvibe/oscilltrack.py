1ae0e419d8934563adcd13ef5e42f6715ca26dd8
harisorgn <organtzh@gmail.com>
2023-02-11 12:30:49 +0000

    complete oscilltrack with stim decision

--- eegvibe/oscilltrack.py
+++ eegvibe/oscilltrack.py
@@ -1,5 +1,9 @@
 import numpy as np
 from math import atan2
+from time import sleep
+import zmq
+from .sound_gen import generate_player
+from .connect import generate_publisher, generate_subscriber
 
 class Oscilltrack:
     a = 0.0
@@ -10,9 +14,13 @@ class Oscilltrack:
     sinv = 0.0
     cosv = 1.0
 
-    def __init__(self, gamma, fc, fs) -> None:
-        self.w = 2 * np.pi * fc / fs
-        self.gamma = gamma
+    def __init__(self, freq_target, phase_target, freq_sample, suppression_cycle, gamma = None):
+        self.phase_target = phase_target
+        self.w = 2 * np.pi * freq_target / freq_sample
+        self.gamma = gamma if gamma != None else 125/freq_sample
+        self.suppression_reset = suppression_cycle * freq_sample / freq_target
+        self.suppression_count = 0
+        self.is_prev_above_thrs = False
 
     def update(self, data):
         
@@ -22,8 +30,8 @@ class Oscilltrack:
 
         self.theta += self.w
         # Wrap theta in the range [-pi, pi] for numerical stability
-        #if self.theta >= np.pi:
-        #   self.theta -= 2*np.pi
+        if self.theta >= np.pi:
+           self.theta -= 2*np.pi
 
         self.sinv = np.sin(self.theta)
         self.cosv = np.cos(self.theta)
@@ -36,5 +44,79 @@ class Oscilltrack:
         # This is a separate function for debug purposes.
         return data - self.re
     
-    def phase(self):
+    def get_phase(self):
         return atan2(self.im, self.re)
+
+    def decide_stim(self):
+        phase = self.get_phase()
+        phase_rotated = phase - self.phase_target
+        if phase_rotated >= np.pi:
+            phase_rotated -= 2*np.pi
+        elif phase_rotated < -np.pi:
+            phase_rotated += 2*np.pi
+
+        is_stim = False
+        is_above_thrs = phase_rotated >= 0
+        
+        if is_above_thrs and (not self.is_prev_above_thrs) and (phase_rotated < np.pi/2):
+            is_stim = self.suppression_count == 0
+            self.suppression_count = self.suppression_reset
+
+        self.is_prev_above_thrs = is_above_thrs
+        if self.suppression_count > 0 :
+            self.suppression_count -= 1
+        else :
+            self.suppression_count = 0
+        
+        return is_stim
+
+
+
+def track_phase(port, topic, port_plot, topic_plot):
+
+    fc = 10
+    t_step = 0.001
+    tracker = Oscilltrack(
+        freq_target=fc, 
+        phase_target=0, 
+        freq_sample=1/t_step, 
+        suppression_cycle=0.8
+    )
+
+    channel = 0
+
+    N_pulses = 1
+    pulse_duration = 100 # ms
+    IPI = 0.0 # sec
+    p = generate_player(N_pulses, pulse_duration, IPI)
+
+    context = zmq.Context()
+    socket = generate_subscriber(port, topic, context)
+    plot_socket = generate_publisher(port_plot, context)
+
+    i = 0
+    while True:
+        topic = socket.recv_string()
+        data = socket.recv_pyobj() 
+        if isinstance(data, str):
+            plot_socket.send_string(topic_plot, zmq.SNDMORE)
+            plot_socket.send_pyobj(data, zmq.SNDMORE)
+            break
+        
+        channel_data = data[0, channel]
+        tracker.update(channel_data)
+        is_stim = tracker.decide_stim()
+
+        if is_stim:
+            p.Play(wait = False)
+
+        plot_socket.send_string(topic_plot, zmq.SNDMORE)
+        plot_socket.send_pyobj(channel_data, zmq.SNDMORE)
+        plot_socket.send_pyobj(is_stim)
+
+        i+=1
+
+    print(f'Analysed {i} samples')
+    sleep(1)  # Gives enough time to the subscribers to update their status
+    socket.close()
+    plot_socket.close()
