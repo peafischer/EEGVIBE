01c8371c49c92b46cff8d0eb2a08d43b7d3a91ed
harisorgn <organtzh@gmail.com>
2023-02-13 16:57:43 +0000

    update test script

--- test_run.py
+++ test_run.py
@@ -1,15 +1,15 @@
 from multiprocessing import Queue, Process, Event
-from threading import Thread
+import threading
 
 from time import sleep, time
 
-from eegvibe import DataIterator, plot_stream, track_phase, write_stream, publisher
+from eegvibe import DataIterator, plot_stream, track_phase, write_stream, publisher, stream_to_queue
 
 if __name__ == '__main__':
     n = 1
     AMP_SR = 1000   # Hz
     channel = 0
-    file = './test_data/tst_10.csv'
+    file = './test_data/tst_simple.csv'
 
     port = 5555
     topic = 'sample'
@@ -20,33 +20,38 @@ if __name__ == '__main__':
 
     pub_queue = Queue()
     stop_event = Event()
-
+    stop_stream_event = threading.Event()
+    
+    #data_thread = Thread(target=data_iter.acquire_data, args=(pub_queue,))
+    #data_thread.start()
+    
+    data_thread = threading.Thread(target=stream_to_queue, args = (pub_queue, stop_stream_event))
+    data_thread.start()
+    
     publisher_process = Process(target=publisher, args=(pub_queue, stop_event, port))
     publisher_process.start()
-
-    data_thread = Thread(target=data_iter.acquire_data, args=(pub_queue,))
-    data_thread.start()
 
     analyzer_process = Process(target=track_phase, args=(port, topic, plot_port, plot_topic))
     analyzer_process.start()
 
-    saver_process = Process(target=write_stream, args=('tst', port, topic))
-    saver_process.start()
+    #saver_process = Process(target=write_stream, args=('tst', port, topic))
+    #saver_process.start()
 
     plot_process = Process(target=plot_stream, args=(plot_port, plot_topic))
     plot_process.start()
     
     t0 = time()
-    while time()-t0 < 3:
+    while time()-t0 < 5:
         print('Still acquiring')
         sleep(1) 
     
     data_iter.stop = True
     pub_queue.put({'topic': 'sample', 'data': 'stop'})
+    stop_stream_event.set()
     data_thread.join()
     analyzer_process.join()
     stop_event.set()
     publisher_process.join()
-    saver_process.join()
+    #saver_process.join()
     plot_process.join()
     print('Bye')
