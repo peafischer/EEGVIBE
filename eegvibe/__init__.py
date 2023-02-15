59f97500cd3f06862eb05a05092f668ec6719dd1
harisorgn <organtzh@gmail.com>
2023-02-15 15:00:52 +0000

    update export for `DataIterator`

--- eegvibe/__init__.py
+++ eegvibe/__init__.py
@@ -1,7 +1,6 @@
 from .filter import *
 from .sound_gen import *
-from .data_iter import DataIterator
 from .oscilltrack import track_phase
-from .read import publisher, stream_to_queue
+from .read import publisher, stream_to_queue, DataIterator
 from .plot import plot_stream
 from .write import write_stream
