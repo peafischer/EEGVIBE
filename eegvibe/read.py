752041661a3a7a651db6e8b503f23181498fdb16
harisorgn <organtzh@gmail.com>
2023-02-15 14:57:14 +0000

    move `DataIterator` to `read.py`

--- eegvibe/read.py
+++ eegvibe/read.py
@@ -1,9 +1,11 @@
 import zmq
 from time import sleep
 from .connect import generate_publisher
-import eego_sdk
+#import eego_sdk
 from threading import Event
 import numpy as np
+import pandas as pd
+import time
 
 def stream_to_queue(queue, event):
     factory = eego_sdk.factory()
@@ -34,3 +36,42 @@ def publisher(queue, event, port):
     socket.send_string('stop')
     sleep(1)  # Gives enough time to the subscribers to update their status
     socket.close()
+
+class DataIterator:
+    def __init__(self, n_samples, sampling_rate, data_file):
+        # channel is assumed to be zero-indexed
+        self.n_samples = n_samples
+        self.sampling_rate = sampling_rate
+        self.counter = 0
+        self.stop = False
+
+        D = np.array(pd.read_csv(data_file))
+        self.data = D[:, :]
+
+        self.n_rows = len(self.data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        idx_start = self.n_samples * self.counter
+        idx_end = idx_start + self.n_samples
+        if idx_end <= self.n_rows:
+            time.sleep(self.n_samples/self.sampling_rate)   
+            self.counter += 1
+
+            if ((self.n_samples + 1) * self.counter) > self.n_rows:
+                self.reset()
+
+            next_data = self.data[idx_start:idx_end]
+            return next_data
+        else:
+            self.counter = 0 # reset iterator
+            raise StopIteration
+
+    def acquire_data(self, queue):
+        while not self.stop:
+            queue.put({'topic': 'sample', 'data': next(self)})
+            
+    def reset(self):
+        self.counter = 0
