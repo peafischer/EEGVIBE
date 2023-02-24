83be490c4b9df8700973adb2edfa22feed850caf
harisorgn <organtzh@gmail.com>
2023-02-24 16:47:06 +0000

    update test script

--- test_run.py
+++ test_run.py
@@ -1,12 +1,12 @@
 from multiprocessing import Queue, Process, Event
 import threading
-
+import pickle
 from time import sleep, time
 
-from eegvibe import DataIterator, plot_stream, track_phase, write_stream, publish_from_queue, stream_to_queue
+from eegvibe import DataIterator, plot_stream, analysis, write_stream, find_filename, stream_to_publish, Stimulator
 
 if __name__ == '__main__':
-    n = 4
+    n = 1
     AMP_SR = 1000   # Hz
     channel = 0
     file = './test_data/tst_10.csv'
@@ -16,28 +16,39 @@ if __name__ == '__main__':
     plot_port = 5556
     plot_topic = 'plot'
 
-    data_iter = DataIterator(n_samples=n, sampling_rate=AMP_SR, data_file=file)
+    out_path = './out_data/'
+    file_data = find_filename(out_path, 'hdf5')
+    file_stim = find_filename(out_path, 'pkl')
 
-    pub_queue = Queue()
-    stop_event = Event()
-    stop_stream_event = threading.Event()
+    data_iter = DataIterator(n_samples=n, sampling_rate=AMP_SR, data_file=file)
     
-    data_thread = threading.Thread(target=data_iter.acquire_data, args=(pub_queue,))
-    data_thread.start()
+    read_thread = threading.Thread(target=data_iter.publish_data, args=(port, topic))
+    read_thread.start()
     
+    #pub_queue = Queue()
+    #stop_event = Event()
+    #stop_stream_event = threading.Event()
+
     #data_thread = threading.Thread(target=stream_to_queue, args = (pub_queue, stop_stream_event))
     #data_thread.start()
-    
-    publisher_process = Process(target=publish_from_queue, args=(pub_queue, stop_event, port))
-    publisher_process.start()
+    #publisher_process = Process(target=publish_from_queue, args=(pub_queue, stop_event, port))
+    #publisher_process.start()
 
-    analyzer_process = Process(target=track_phase, args=(port, topic, plot_port, plot_topic), kwargs={'channels_ms' : range(0,2), "channels_EMG" : [1,2]})
-    analyzer_process.start()
+    analysis_process = Process(
+        target=analysis, 
+        args=(port, topic, plot_port, plot_topic, file_stim), 
+        kwargs={'channels_ms' : range(0,2), "channels_EMG" : [1,2], "stim_device_ID" : 1}
+    )
+    analysis_process.start()
 
-    saver_process = threading.Thread(target=write_stream, args=(port, topic))
+    saver_process = threading.Thread(target=write_stream, args=(port, topic, file_data))
     saver_process.start()
 
-    plot_process = Process(target=plot_stream, args=(plot_port, plot_topic), kwargs={'labels' : ["Track", "EMG1", "EMG2"]})
+    plot_process = Process(
+        target=plot_stream, 
+        args=(plot_port, plot_topic),
+        kwargs={'n_samples' : 1000, 'labels' : ["Track", "EMG1", "EMG2"]}
+    )
     plot_process.start()
     
     t0 = time()
@@ -46,12 +57,19 @@ if __name__ == '__main__':
         sleep(1) 
     
     data_iter.stop = True
-    pub_queue.put({'topic': 'sample', 'data': 'stop'})
-    stop_stream_event.set()
-    data_thread.join()
-    analyzer_process.join()
-    stop_event.set()
-    publisher_process.join()
-    saver_process.join()
+    #pub_queue.put({'topic': 'sample', 'data': 'stop'})
+    #stop_stream_event.set()    
+    #stop_event.set()
+    #publisher_process.join()
     plot_process.join()
-    print('Bye')
+    saver_process.join()
+    analysis_process.join()
+    read_thread.join()
+
+    sleep(2)
+    print('Starting replay')
+    with open(file_stim, 'rb') as f:
+        d = pickle.load(f)
+
+    s = Stimulator(N_pulses = d['N_pulses'], IPI = d['IPI'], pulse_duration = d['pulse_duration'], device_ID = d['device_ID'], stim_times = d['stim_times'])
+    s.replay()
