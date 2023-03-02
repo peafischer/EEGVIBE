911ee61eac4f9d838bf6a3be577b971bb26d09c0
harisorgn <organtzh@gmail.com>
2023-03-02 13:18:58 +0000

    update pipeline running test

--- test_run.py
+++ test_run.py
@@ -2,13 +2,14 @@ from eegvibe import run_tracking, run_replay
 from time import sleep
 
 if __name__ == '__main__':
-    run_tracking(freq_sample = 1000, freq_target = 50, phase_target = 0.0, freq_high_pass = 8, oscilltrack_suppresion = 0.8,
-        is_CL_stim = True, N_pulses = 10, pulse_duration = 0.1, ITI = 0.5, IPI = 0.2, stim_device_ID = 2,
-        channel_track = 14, channels_ref = range(0,31), channels_EMG = [32,33],
-        N_plot_samples = 1000, plot_labels = ["Track", "EMG1", "EMG2"], recording_duration = 10,
-        subject_ID = 4
+    run_tracking(freq_sample = 1000, freq_target = 10, phase_target = 0.0, freq_high_pass = 3, oscilltrack_suppresion = 0.8,
+        is_CL_stim = False, N_pulses = 5, pulse_duration = 0.05, ITI = 1.0, IPI = 0.1, stim_device_ID = 1,
+        channel_track = 14, channels_ref = range(0,31), channels_EMG = [32],
+        N_plot_samples = 1000, plot_labels = ["Track", "EMG1"], recording_duration = 10,
+        subject_ID = 1, filename_data = './test_data/eeg_rest.csv'
         )
     
+    """
     sleep(5)
     print('Starting replay')
 
@@ -17,3 +18,4 @@ if __name__ == '__main__':
         channel_track = 14, channels_ref = range(0,31), channels_EMG = [32,33],
         N_plot_samples = 1000, plot_labels = ["Track", "EMG1", "EMG2"], recording_duration = 10
         )
+    """
