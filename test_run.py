e61f114fd516e317ef0b7d9b9a1c210bdc95e605
harisorgn <organtzh@gmail.com>
2023-03-03 13:47:39 +0000

    update test script

--- test_run.py
+++ test_run.py
@@ -9,14 +9,13 @@ if __name__ == '__main__':
         participant_ID = 1, filename_data = './test_data/eeg_rest.csv', condition_label = 'tst'
         )
     
-    """
     sleep(5)
     print('Starting replay')
 
     run_replay(freq_sample = 1000, freq_high_pass = 3,
-        is_CL_stim = False, filename_stim = './out_data/03_03_2023_P1_Ch14_FRQ=10Hz_SEMI_CL_phase=0.0_tst_v2.pkl',
+        is_CL_stim = False, filename_stim = './out_data/03_03_2023_P1_Ch14_FRQ=10Hz_SEMI_CL_phase=0.0_tst_v1.pkl',
         channel_track = 14, channels_ref = range(0,31), channels_EMG = [32,33],
         N_plot_samples = 1000, plot_labels = ["Track", "EMG1", "EMG2"], recording_duration = 10,
         participant_ID = 1, filename_data = './test_data/eeg_rest.csv'
         )
-    """
+    
