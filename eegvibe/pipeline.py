a4cfa3422d69d911b6041703504d97d47a3c237b
harisorgn <organtzh@gmail.com>
2023-03-03 13:30:48 +0000

    update pipeline for track and replay

--- eegvibe/pipeline.py
+++ eegvibe/pipeline.py
@@ -1,36 +1,59 @@
 from multiprocessing import Process
 import threading
-from time import sleep, time
+from time import sleep
 
-from eegvibe import (DataIterator, plot_stream, tracking, replay, write_stream, find_filename, HighPassFilter, Oscilltrack,
-                     stream_to_publish, CLStimulator, init_CLStimulator, SemiCLStimulator, init_SemiCLStimulator)
-
+from .read import DataIterator, stream_to_publish, get_freq_sample, get_impedance
+from .analysis import tracking, replay
+from .filter import HighPassFilter
+from .oscilltrack import Oscilltrack
+from .stimulate import CLStimulator, SemiCLStimulator, init_CLStimulator, init_SemiCLStimulator
+from .write import write_stream, find_filename, add_metadata
+from .plot import plot_stream
 
 def run_tracking(freq_sample, freq_target, phase_target, freq_high_pass, oscilltrack_suppresion,
         is_CL_stim, N_pulses, pulse_duration, ITI, IPI, stim_device_ID,
         channel_track, channels_ref, channels_EMG,
-        subject_ID, N_plot_samples, plot_labels, recording_duration = 10,
-        filename_data = None):
+        N_plot_samples, plot_labels, participant_ID, recording_duration = 10,
+        out_path = './out_data/', condition_label = '', filename_data = None,
+        amplifier_ID = 0):
 
     port = 5555
     topic = 'sample'
     plot_port = 5556
     plot_topic = 'plot'
 
-    out_path = './out_data/'
-    file_data = find_filename(subject_ID, out_path, 'hdf5')
-    file_stim = find_filename(subject_ID, out_path, 'pkl')
+    stop_stream_event = threading.Event()
+    if filename_data is None:
+        final_freq_sample = get_freq_sample(freq_sample, amplifier_ID)
+        impedance_init = get_impedance(amplifier_ID)
+
+        read_thread = threading.Thread(
+            target=stream_to_publish, 
+            args=(final_freq_sample, stop_stream_event, port, topic)
+        )
+    else:
+        final_freq_sample = freq_sample
+        impedance_init = [0.0 for _ in range(0,32)]
+
+        data_iter = DataIterator(
+            n_samples = 8, 
+            freq_sample = final_freq_sample, 
+            data_file = filename_data
+        )
+        read_thread = threading.Thread(
+            target = data_iter.publish_data, 
+            args = (port, topic)
+        )
     
-    # TO DO :
-    # change freq_sample to final_freq_sample after sampling frequency is resolved via get_freq_sample()
     tracker = Oscilltrack(
         freq_target = freq_target, 
         phase_target = phase_target, 
-        freq_sample = freq_sample, 
+        freq_sample = final_freq_sample, 
         suppression_cycle = oscilltrack_suppresion
     )
 
     if is_CL_stim: 
+        sm = 'FULL_CL'
         stim = CLStimulator(
             N_pulses_stim = 1, 
             N_stim_train = N_pulses, 
@@ -40,6 +63,7 @@ def run_tracking(freq_sample, freq_target, phase_target, freq_high_pass, oscillt
             device_ID = stim_device_ID
         )
     else:
+        sm = 'SEMI_CL'
         stim = SemiCLStimulator(
             N_pulses = N_pulses, 
             ITI = ITI, 
@@ -50,86 +74,140 @@ def run_tracking(freq_sample, freq_target, phase_target, freq_high_pass, oscillt
 
     filter_track = HighPassFilter(
         freq_corner = freq_high_pass, 
-        freq_sample = freq_sample
+        freq_sample = final_freq_sample
     )
-    filters_EMG = [HighPassFilter(freq_corner = freq_high_pass, freq_sample = freq_sample) for _ in range(len(channels_EMG))]
+    filters_EMG = [
+        HighPassFilter(freq_corner = freq_high_pass, freq_sample = final_freq_sample) 
+        for _ in range(len(channels_EMG))
+    ]
+
+    filename_out_data = find_filename(
+        participant_ID = participant_ID, 
+        channel_track = channel_track,
+        freq_target = freq_target,
+        stim_mode = sm,
+        phase_target = phase_target,
+        label = condition_label,
+        path = out_path,
+        format = 'hdf5'
+        )    
+    
+    fs = filename_out_data.split('.')
+    filename_stim = '.'.join(fs[:-1]) + '.pkl'
 
     analysis_process = Process(
         target=tracking, 
         args=(port, topic, plot_port, plot_topic, tracker, stim, filter_track, filters_EMG), 
         kwargs={
-            'filename_stim' : file_stim, 
+            'filename_stim' : filename_stim, 
             'channel_track' : channel_track, 
             'channels_ref' : channels_ref, 
             'channels_EMG' : channels_EMG
         }
     )
-    analysis_process.start()
 
-    saver_process = threading.Thread(target=write_stream, args=(port, topic, file_data))
-    saver_process.start()
+    saver_process = threading.Thread(
+        target = write_stream, 
+        args = (port, topic, filename_out_data)
+    )
 
     plot_process = Process(
-        target=plot_stream, 
-        args=(plot_port, plot_topic),
-        kwargs={'n_samples' : N_plot_samples, 'labels' : plot_labels}
+        target = plot_stream, 
+        args = (plot_port, plot_topic),
+        kwargs = {'n_samples' : N_plot_samples, 'labels' : plot_labels}
     )
-    plot_process.start()
-    
-    stop_stream_event = threading.Event()
-    if filename_data is None:
-        read_thread = threading.Thread(target=stream_to_publish, args=(freq_sample, stop_stream_event, port, topic))
-        read_thread.start()
-    else:
-        data_iter = DataIterator(
-            n_samples = 8, 
-            freq_sample = freq_sample, 
-            data_file = filename_data
-        )
-        read_thread = threading.Thread(target=data_iter.publish_data, args=(port, topic))
-        read_thread.start()
-    
 
-    t0 = time()
-    while time()-t0 < recording_duration:
-        print('Still acquiring')
-        sleep(1) 
+    analysis_process.start()
+    saver_process.start()
+    plot_process.start()
+    read_thread.start()
+
+    sleep(recording_duration) 
     
-    data_iter.stop = True
-    stop_stream_event.set()    
+    if filename_data is None:
+        stop_stream_event.set()
+        impedance_final = get_impedance(amplifier_ID)
+    else:
+        data_iter.stop = True
+        impedance_final = [1.0 for _ in range(0,32)]
+
     plot_process.join()
     saver_process.join()
     analysis_process.join()
     read_thread.join()
+    
+    metadata_dict = {
+        'impedance_init' : impedance_init,
+        'impedance_final' : impedance_final,
+        'freq_sample' : final_freq_sample,
+        'recording_duration' : recording_duration,
+        'oscilltrack_suppression' : oscilltrack_suppresion,
+        'replay_file' : '',
+        'participant_ID' : participant_ID,
+        'stim_mode' : sm,
+        'N_pulses_per_train' : N_pulses,
+        'pulse_duration' : pulse_duration,
+        'IPI' : 0.0 if is_CL_stim else IPI,
+        'ITI' : ITI,
+        'phase_target' : phase_target,
+        'freq_target' : freq_target,
+        'condition_label' : condition_label
+    }
+    add_metadata(metadata =  metadata_dict, filename = filename_out_data)
 
-
-def run_replay(freq_sample, freq_target, phase_target, freq_high_pass, oscilltrack_suppresion,
-        is_CL_stim, filename_stim,
+def run_replay(freq_sample, freq_high_pass, is_CL_stim, filename_stim,
         channel_track, channels_ref, channels_EMG,
-        N_plot_samples, plot_labels, recording_duration = 10):
-    n = 1
-    file = './test_data/tst_10.csv'
-
+        N_plot_samples, plot_labels, participant_ID, recording_duration = 10,
+        condition_label = '', filename_data = None, amplifier_ID = 0):
+    
     port = 5555
     topic = 'sample'
     plot_port = 5556
     plot_topic = 'plot'
 
     fs = filename_stim.split('.')
-    file_data = '.'.join(fs[:-1]) + 'REPLAY' + '.hdf5'
+    filename_out_data = '.'.join(fs[:-1]) + '_REPLAY' + '.hdf5'
 
-    data_iter = DataIterator(n_samples=n, sampling_rate=freq_sample, data_file=file)
-    
-    if is_CL_stim: 
-        stim = init_CLStimulator(filename_stim)
+    stop_stream_event = threading.Event()
+    if filename_data is None:
+        final_freq_sample = get_freq_sample(freq_sample, amplifier_ID)
+        impedance_init = get_impedance(amplifier_ID)
+
+        read_thread = threading.Thread(
+            target = stream_to_publish, 
+            args = (final_freq_sample, stop_stream_event, port, topic)
+        )
     else:
+        final_freq_sample = freq_sample
+        impedance_init = [0.0 for _ in range(0,32)]
+
+        data_iter = DataIterator(
+            n_samples = 8, 
+            freq_sample = final_freq_sample, 
+            data_file = filename_data
+        )
+        read_thread = threading.Thread(
+            target = data_iter.publish_data, 
+            args = (port, topic)
+        )
+        
+    if is_CL_stim: 
+        sm = 'FULL_CL'
+        stim = init_CLStimulator(filename_stim)
+        N_pulses = stim.N_stim_train
+    else:
+        sm = 'SEMI_CL'
         stim = init_SemiCLStimulator(filename_stim)
+        N_pulses = stim.N_pulses
 
     filter_track = HighPassFilter(
         freq_corner = freq_high_pass, 
-        freq_sample = freq_sample
+        freq_sample = final_freq_sample
     )
-    filters_EMG = [HighPassFilter(freq_corner = 8, freq_sample = 1000) for _ in range(len(channels_EMG))]
+    filters_EMG = [
+        HighPassFilter(freq_corner = freq_high_pass, freq_sample = final_freq_sample) 
+        for _ in range(len(channels_EMG))
+    ]
 
     analysis_process = Process(
         target=replay, 
@@ -140,33 +218,49 @@ def run_replay(freq_sample, freq_target, phase_target, freq_high_pass, oscilltra
             'channels_EMG' : channels_EMG
         }
     )
-    analysis_process.start()
 
-    saver_process = threading.Thread(target=write_stream, args=(port, topic, file_data))
-    saver_process.start()
-
-    plot_process = Process(
-        target=plot_stream, 
-        args=(plot_port, plot_topic),
-        kwargs={'n_samples' : N_plot_samples, 'labels' : plot_labels}
+    saver_process = threading.Thread(
+        target = write_stream, 
+        args = (port, topic, filename_out_data)
     )
+    
+    plot_process = Process(
+        target = plot_stream, 
+        args = (plot_port, plot_topic),
+        kwargs = {'n_samples' : N_plot_samples, 'labels' : plot_labels}
+    )
+
+    analysis_process.start()
+    saver_process.start()
     plot_process.start()
-    
-    #read_thread = threading.Thread(target=data_iter.publish_data, args=(port, topic))
-    #read_thread.start()
-    
-    stop_stream_event = threading.Event()
-    read_thread = threading.Thread(target=stream_to_publish, args=(freq_sample, stop_stream_event, port, topic))
     read_thread.start()
+
+    sleep(recording_duration) 
     
-    t0 = time()
-    while time()-t0 < recording_duration:
-        print('Still acquiring')
-        sleep(1) 
-    
-    data_iter.stop = True
-    stop_stream_event.set()    
+    if filename_data is None:
+        stop_stream_event.set()
+        impedance_final = get_impedance(amplifier_ID)
+    else:
+        data_iter.stop = True
+        impedance_final= [1.0 for _ in range(0,32)]
+
     plot_process.join()
     saver_process.join()
     analysis_process.join()
     read_thread.join()
+
+    metadata_dict = {
+        'impedance_init' : impedance_init,
+        'impedance_final' : impedance_final,
+        'freq_sample' : final_freq_sample,
+        'recording_duration' : recording_duration,
+        'replay_file' : filename_stim,
+        'participant_ID' : participant_ID,
+        'stim_mode' : sm,
+        'N_pulses_per_train' : N_pulses,
+        'pulse_duration' : stim.pulse_duration,
+        'IPI' : 0.0 if is_CL_stim else stim.IPI,
+        'ITI' : stim.ITI,
+        'condition_label' : condition_label
+    }
+    add_metadata(metadata =  metadata_dict, filename = filename_out_data)
