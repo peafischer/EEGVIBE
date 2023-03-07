42f770cd0e785d9c264948cb07b4b16fdec6f76d
harisorgn <organtzh@gmail.com>
2023-03-07 11:25:48 +0000

    cleanup filter

--- eegvibe/filter.py
+++ eegvibe/filter.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def mean_subtract(data_all, data_channel):
-    d = data_channel - np.mean(data_all)
-    return d
+    return data_channel - np.mean(data_all)
 
 class HighPassFilter:
     x = 0.0
@@ -11,8 +10,6 @@ class HighPassFilter:
         self.hp = 2.0 * np.pi * freq_corner / freq_sample
 
     def filter(self, data):
-        #filt_data = data - self.x
-        data -= self.x
-        #self.x += self.hp * filt_data
-        self.x += self.hp * data
-        #return filt_data
+        filt_data = data - self.x
+        self.x += self.hp * filt_data
+        return filt_data
