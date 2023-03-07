7c31826515bbbd452fe7a76928fb27b13915976c
harisorgn <organtzh@gmail.com>
2023-03-07 11:24:51 +0000

    update exports

--- eegvibe/__init__.py
+++ eegvibe/__init__.py
@@ -2,7 +2,7 @@ from .filter import *
 from .stimulate import Stimulator, CLStimulator, init_CLStimulator, SemiCLStimulator, init_SemiCLStimulator
 from .oscilltrack import Oscilltrack
 from .analysis import tracking, replay
-from .read import stream_to_publish, DataIterator, get_freq_sample
+from .read import read_from_stream, read_from_file, get_freq_sample
 from .plot import plot_stream
-from .write import write_stream, find_filename
+from .write import write_stream, find_filename, is_CL_stim
 from .pipeline import run_tracking, run_replay
